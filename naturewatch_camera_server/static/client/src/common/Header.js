14fe9865a033d1e7262992b8cca1ac685064c2ab
Carl Venter <carlventer@live.co.za>
2024-04-19 23:47:25 +0200

    Check build

--- naturewatch_camera_server/static/client/src/common/Header.js
+++ naturewatch_camera_server/static/client/src/common/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Header extends React.Component {
     render() {
         return(
-            <h1>My Naturewatch</h1>
+            <h1>Hello Stephan!</h1>
         );
     }
 }
